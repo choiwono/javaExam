068588d95612ae345983cebb699326a968e2111a
choiwono <treasure_b@naver.com>
2019-01-21 15:41:21 +0900

    #19.01.21 파일 삭제

--- ServletSample/src/main/java/my/examples/jdbcboard/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package my.examples.jdbcboard.service;
-
-import my.examples.jdbcboard.dto.User;
-
-public interface UserService {
-    void addUser(User user);
-
-    User getUserByEmail(String email);
-}
